7e418c1464ffe36693e377c617057169f430e6e8
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-20 20:01:05 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Crazy from './components/Crazy';
 
 export default function App() {
+  const pox = ['find', 'the', 'value'];
+  Function;
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Crazy />
     </div>
   );
 }
---
+++ src/components/Crazy.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Crazy () {
+  const kli =[1,2,3]
+  const kjh = kli.map((kli)=>kli*2)
+  console.log(kjh)
+  return(
+    <div>
+      <p>
+        whats that value
+        </p>
+      </div>
+  )
+
+}
+
+
+export default Crazy;
